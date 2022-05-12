5ac18721031655ab658257ab9c71c22bec0d1d3b
John Doe <johndoe@email.com>
2022-05-13 05:38:17 +0700

    adding new project

---
+++ miniproject-laptop/src/components/Hero.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="row mt-4">
+      <div class="col">
+          <div class="d-flex h-100">
+              <div class="justify-content-center align-self-center">
+
+                  <h2><strong>Welcome</strong><br>Place for your daily drive with best deal</h2>
+                  <!-- <button class="btn btn-lg btn-success">Buy Now</button> -->
+                   <b-button variant="primary" size="lg">Buy Now</b-button>
+              </div>
+          </div>
+      </div>
+      <div class="col">
+
+          <img src="../assets/img/model1.jpg" width="100%"/>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Hero'
+}
+</script>
+
+<style>
+
+</style>
---
+++ miniproject-laptop/src/components/Hero2.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="row">
+        <div class="col">
+            <h2><strong>DevGear Product</strong></h2>
+        </div>
+        <div class="d-flex h-100">
+              <div class="justify-content-center align-self-center">
+  <b-dropdown text="Primary" variant="primary" class="ml-10">
+    <b-dropdown-item href="#">Action</b-dropdown-item>
+    <b-dropdown-item href="#">Another action</b-dropdown-item>
+    <b-dropdown-item href="#">Something else here</b-dropdown-item>
+  </b-dropdown>
+  </div>
+        </div>
+  </div>
+  
+</template>
+
+<script>
+export default {
+    name: 'Hero2'
+}
+</script>
+
+<style>
+
+</style>
