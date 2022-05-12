0b11217df5944baedc62c71ff3c77615f7c3828a
John Doe <johndoe@email.com>
2022-05-13 06:02:09 +0700

    adding footer

---
+++ miniproject-laptop/src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template> 
+ <footer>
+  <div> 
+   <p>amith</p>
+  </div> 
+ </footer>
+</template> 
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
+
+<style scoped> 
+footer { 
+display: flex; 
+background-color: #42b942; 
+height: 50px; 
+width: 100%; 
+position: fixed; 
+} 
+</style> 
