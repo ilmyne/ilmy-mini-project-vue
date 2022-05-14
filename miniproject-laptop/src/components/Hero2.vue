ad8cdfc1bb8cec602e7572824bbbb2f0172e8049
John Doe <johndoe@email.com>
2022-05-14 20:38:08 +0700

    edit dropdown

--- miniproject-laptop/src/components/Hero2.vue
+++ miniproject-laptop/src/components/Hero2.vue
@@ -1,27 +1,29 @@
+<!-- /* eslint-disable vue/no-parsing-error */ -->
 <template>
   <div class="row">
-        <div class="col">
-            <h2><strong>DevGear Product</strong></h2>
-        </div>
-        <div class="d-flex h-100">
-              <div class="justify-content-center align-self-center">
-  <b-dropdown text="Primary" variant="primary" class="ml-10">
-    <b-dropdown-item href="#">Action</b-dropdown-item>
-    <b-dropdown-item href="#">Another action</b-dropdown-item>
-    <b-dropdown-item href="#">Something else here</b-dropdown-item>
-  </b-dropdown>
+    <div class="col">
+      <h2><strong>DevGear Product</strong></h2>
+
+      <b-dropdown
+          text="Choose your preference"
+          id="dropdown-left"
+          variant="primary"
+          class="float-left"
+        >
+          <b-dropdown-item href="#">ASUS</b-dropdown-item>
+          <b-dropdown-item href="#">Lenovo</b-dropdown-item>
+          <b-dropdown-item href="#">Samsung</b-dropdown-item>
+        </b-dropdown>
+
+    </div>
   </div>
-        </div>
-  </div>
-  
 </template>
 
 <script>
 export default {
-    name: 'Hero2'
-}
+  name: "Hero2",
+};
 </script>
 
 <style>
-
 </style>
